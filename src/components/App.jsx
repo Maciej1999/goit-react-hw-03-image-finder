c30d6d6eeb50eaa32244c44a901833702916e278
Maciej Sikora <109343822+Maciej1999@users.noreply.github.com>
2023-07-20 21:53:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export class App extends Component {
       modalIndex: '-1',
       loading: false,
       loadingMore: false,
-      query: 'Pokemon Charizard',
+      query: 'Pokemon',
       page: 0,
       scroll: 0,
     };
